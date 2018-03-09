51343b8a0f9c9116844c391f523a1574bbddf0f3
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-03-09 19:28:06 +0100

    Create pickle_gestor.py

---
+++ pickle_gestor.py
@@ -0,0 +1,96 @@
+import pickle
+from VALID import ns, opt, OKI
+import time
+import subprocess
+
+def conte(x):
+    while True:
+        nombre=input("Introduzca el nombre del archivo cuyo contenido quiere visualizar: ")
+        try:
+            contenido=pickle.load(open(nombre,"rb"))
+            break
+        except:
+            print("NO SE ENCONTRO EL ARCHIVO ",nombre)
+    if x==("a"):
+        return contenido
+    else:
+        return(nombre)
+        
+
+def dat(n):
+    try:
+        n=int(n)
+    except:
+        try:
+            n=float(n)
+        except:
+            n=str(n)
+    return n
+
+while True:
+    print("Escola una opción.")
+    print("A)CREAR NUEVO ARCHIVO.")
+    print("B)VER UN ARCHIVO.")
+    print("C)INTRODUCIR CAMBIOS EN UN ARCHIVO.")
+    op=opt(input("Introduzca aquí su opción: "),["A","B","C"])
+    if op==("A"):
+        lista=[]
+        contenido=input("introduzca dato/s separados por coma: ")
+        un=("").join(contenido)
+        sep=un.split(",")
+        for i in sep:
+            elem=dat(i)
+            print(type(elem))
+            lista.append(elem)
+        print(lista)
+        nombreA=input("nuevo archivo: ")
+        pickle.dump(lista,open(nombreA,"wb"))
+        print(lista)
+        print("El archivo se creo correctamente")
+        
+    elif op==("B"):
+        contenido=conte("a")
+        print("")
+        print(contenido)
+        print("")
+    elif op==("C"):
+        nombreA=conte("b")
+        nombre=pickle.load(open(nombreA,"rb"))
+        print("¿Que tipo de cambio es el que desea realizar?")
+        print("A)AÑADIR CAMPOS")
+        print("B)ELIMINAR CAMPOS")
+        numcam=(len(nombre))-1
+        n=0
+        op=opt(input("Introduzca aquí su opción: "),["A","B"])
+        if op==("A"):
+            numero_campos=OKI(input("Introduzca el número de campos que desea añadir: "))
+            cam_nuevos=[]
+            while n<numero_campos:#DUPLICADO
+                nombre.append(0)
+                numcam+=1
+                cam_nuevos.append(numcam)
+                n+=1
+            print("")
+            print("NUEVO ESTADO: ",nombre)
+            print("CAMPOS AÑADIDOS: ",cam_nuevos)
+            print("")
+        elif op==("B"):
+            numero_campos=OKI(input("Introduzca el número de campos a eliminar: "))
+            while numero_campos>len(nombre):
+                numero_campos=OKI(input("El número de campos introducido es superior al número de campos actual: "))
+            cam_eliminad=[]
+            while n<numero_campos:#DUPLICADO
+                cam_eliminad.append(numcam)
+                numcam-=1
+                del nombre[-1]
+                n+=1
+            cam_eliminad.sort()
+            print("")
+            print("NUEVO ESTADO: ",nombre)
+            print("CAMPOS ELIMINADOS: ",cam_eliminad)
+            print("")
+        pickle.dump(nombre,open(nombreA,"wb"))      
+    conti=ns(input("¿Desea continuar?: "))
+    if conti==("n"):
+        break
+    subprocess.call(["cmd.exe","/C","cls"])
