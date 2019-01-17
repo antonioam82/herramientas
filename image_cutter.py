95bb4a65b1b99a20a650f83ff0d1fe553f032ec1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-01-17 16:10:05 +0100

    Update image_cutter.py

--- image_cutter.py
+++ image_cutter.py
@@ -6,23 +6,27 @@ import os
 while True:
     puntos=[]
     inicial=input("Introduce inicial: ")
+    
     pixels=input("Introduce medidas: ")
     pix=pixels.split(",")
     for i in pix:
         puntos.append(int(i))
+            
     box=(puntos[0],puntos[1],puntos[2],puntos[3])
-    print(box)
+
     for file in os.listdir():
         if file.startswith(inicial):
             try:
                 imagen = Image.open(file)
+                ig=imagen
                 n_imagen = imagen.crop(box)
                 n_imagen.save(file)
             except:
                 print("La operación no pudo completarse con éxito")
-                pass
+                ig.save(file)
+                break
     conti=ns(input("¿Continuar?: "))
 
     if conti=="n":
         break
-    subprocess.cal(["cmd.exe","/C","cls"])
+    subprocess.call(["cmd.exe","/C","cls"])
