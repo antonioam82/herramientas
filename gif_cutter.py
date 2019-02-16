8cff293969c0662114b6cfee558565c5ac5ae652
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-02-16 21:07:43 +0100

    Create gif_cutter.py

---
+++ gif_cutter.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import os
+from VALID import ns, OKI
+
+os.chdir(r'C:\Users\Antonio\Documents\Nueva carpeta\imagess')
+
+while True:
+
+    print("")
+    print("_____________________________")
+    print("|                           |")
+    print("|       --GIF CUTTER--      |")
+    print("|___________________________|")
+    print("")
+
+    giff=input("Nobre del archivo: ")
+    gif_name=giff+".gif"
+
+    dato_iz=OKI(input("Introduce dato izquierdo: "))
+    dato_sup=OKI(input("Introduce dato superior: "))
+    dato_der=OKI(input("Introduce dato derecho: "))
+    dato_inf=OKI(input("Introduce dato inferior: "))
+    
+    box=(dato_iz, dato_sup, dato_der, dato_inf)
+    count=0
+    
+    im = Image.open(gif_name)
+    count+=1
+    file=giff+str(count)
+    n_imagen = im.crop(box)
+    n_imagen.save(file+".gif")
+    
+    im.seek(0)
+    try:
+        while 1:
+            im.seek(im.tell()+1)
+            count+=1
+            file=giff+str(count)
+            n_imagen = im.crop(box)
+            n_imagen.save(file+".gif")
+    except EOFError:
+        pass
+
+    conti=ns(input("¿Continuar?: "))
+    if conti=="n":
+        break
