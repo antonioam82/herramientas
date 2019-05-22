4ab36b6049fbcbab50b44014da43f6fb1704aa31
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-05-22 16:33:26 +0200

    Update im_info.py

--- im_info.py
+++ im_info.py
@@ -10,12 +10,16 @@ while True:
         break
     else:
         if "." in dato:
-            im=Image.open(dato)
+            try:
+                im=Image.open(dato)
+            except:
+                print("Nope")
         else:
             sep=dato.split(",")
             im.convert(sep[0])
-            print(sep)
             #if sep[0]=="P" and len(sep)==1:
+                #im.convert('P')
+                #print(im)
                 #output=np.array(im.getpalette())
             if sep[1]=="R":
                 output=np.array(im.getchannel(0))
